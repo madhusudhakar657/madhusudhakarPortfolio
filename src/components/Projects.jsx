f9d0c0c4a4b9c9d9f4f28ff057a570a2cb63f603
madhusudhakar657 <madhusudhakar657@gmail.com>
2024-10-07 13:44:37 +0530

    projects added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,21 +1,23 @@
 import React from "react";
+import AmazonImage from "../assets/amazon_PNG.png";
 import employeeMSImage from "../assets/employee-ms.png";
-import bookMSImage from "../assets/admin-dashboard.png";
+
+// import bookMSImage from "../assets/admin-dashboard.png";
 
 const projects = [
   {
     id: 1,
-    name: "Employee MS",
-    technologies: "MERN Stack",
-    image: employeeMSImage,
-    github: "https://github.com/YouafKhan1",
+    name: "AmazingShop",
+    technologies: "React Js",
+    image: AmazonImage,
+    github: "https://madhusudhakar657.github.io/amazingShop",
   },
   {
     id: 2,
-    name: "Blog App",
+    name: "MACADEMY",
     technologies: "MERN Stack",
-    image: bookMSImage,
-    github: "https://github.com/YouafKhan1",
+    image: "https://macademy-profiles.s3.ap-south-1.amazonaws.com/uploads/0macademy-logo.jpg",
+    github: "https://testing-admin.macademy.in/",
   },
   {
     id: 3,
@@ -36,12 +38,12 @@ const Projects = () => {
             <div key={project.id} className="bg-gray-800 p-6 rounded-lg hover:shadow-lg 
             transform transition-transform duration-300 hover:scale-105">
               <img src={project.image} alt={project.name} className="rounded-lg mb-4 
-              w-full h-48 object-cover" />
+              w-auto h-48 object-cover" />
               <h3 className="text-2xl font-bold mb-2">{project.name}</h3>
               <p className="text-gray-400 mb-4">{project.technologies}</p>
               <a href={project.github} className="inline-block bg-gradient-to-r 
               from-green-400 to-blue-500 text-white px-4 py-2 rounded-full" target="_blank" 
-              rel="noopener noreferrer">GitHub</a>
+              rel="noopener noreferrer">Click To See</a>
             </div>
           ))}
         </div>
